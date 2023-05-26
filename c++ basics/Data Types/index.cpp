601b08a81b6938d12d90ba395c791194b8e0870e
Shiva <shivaxwarrior@gmail.com>
2023-05-26 17:00:20 +0530

    Some changes

--- c++ basics/Data Types/index.cpp
+++ c++ basics/Data Types/index.cpp
@@ -1,4 +1,4 @@
-/* In C++, data types specify the type of data that a variable can hold. Each data type has a specific range of values and operations that can be performed on it. Here are some commonly used data types in C++: 
+/* In C++, data types specify the type of data that a variable can hold. Each data type has a specific range of values and operations that can be performed on it. Here are some commonly used data types in C++:
 
 1.Primitive Data Types:
 int: Represents integer values.
@@ -10,28 +10,35 @@ bool: Represents boolean values (true or false).
 example shown here
 */
 
-#include<iostream>
-int main(){
+#include <iostream>
+using namespace std; // used it so that i dont have to write std::string or std::getline
+int main()
+{
     int age = 25;
     float temperature = 104.1;
     double pi = 3.14159;
-    char grade = 'A' ;//Char as a value o 8 bits i.e 1 byte and it can store only 1 character by default in order to store a complete name or a word we have to use string
+    char grade = 'A';  // Char as a value o 8 bits i.e 1 byte and it can store only 1 character by default in order to store a complete name or a word we have to use string
+    string str;        // however it cant store the spaces in order to store a sentence that has spaces we have to use getline
+    getline(cin, str); // If we dont use getline we will have to assign different values to variables like
+    string idk = "Name"; // in string we use " " and in char we use ' '
+    string a, b, c;
+    cin >> a >> b >> c; //it will end up taking a heck lot of space 
+    cout << a << b << c;
+    cout << str;
+
     bool status = true;
-}
 
-/*
-2. Modifier Data Types:
-unsigned: Used with integer types to represent non-negative values only.
-long: Extends the range of integer types.
-short: Reduces the range of integer types.
-*/
-#include<iostream>
-int main(){
-    unsigned int count = 10; //stores only positive value why do we need it ? lets say we need to store the age of a person in a variable we can use unsgined int as users age cannot be in negative
+    /*
+    2. Modifier Data Types:
+    unsigned: Used with integer types to represent non-negative values only.
+    long: Extends the range of integer types.
+    short: Reduces the range of integer types.
+    */
+    unsigned int count = 10; // stores only positive value why do we need it ? lets say we need to store the age of a person in a variable we can use unsgined int as users age cannot be in negative
 
-    signed int number = 377; //its  the opposite of unsigned int signed only stores positive numbers by default when we initialize an integrer its signed 
+    signed int number = 377; // its  the opposite of unsigned int signed  stores both positive and negative numbers, by default when we initialize an integrer its signed
 
-    // before knowing about long int , long long int, short int, we need to know the range 
+    // before knowing about long int , long long int, short int, we need to know the range
 
     /*
     the size of integers is compiler dependent we will be using the data of mingw
@@ -41,13 +48,13 @@ int main(){
     int :- consits of 4 bytes i.e 32bits
 
     long int :- consits of 4 bytes or 8 bytes (depending on system) i.e 32 or 64bits
-    
-    long long int :- consits of 8 bytes i.e 64bits 
+
+    long long int :- consits of 8 bytes i.e 64bits
 
     short int :- consists of 2 bytes i.e 16bits i
 
 
-    Now lets learn the range of signed and unsigined 
+    Now lets learn the range of signed and unsigined
 
     calculate the range of signed integers
 
@@ -55,7 +62,7 @@ int main(){
 
     range of unsigned int is 0 to 2^n-1
 
-    lets calculate now 
+    lets calculate now
 
     SIGNED INTEGRES
 
@@ -76,10 +83,9 @@ int main(){
     size of signed long long int = 0 to 2^(64)-1 =  -9223372036854775808 to 9223372036854775807.
 
     size of signed short int = 0 to 2^(16)-1 =  0 to 65535.
-    */ 
+    */
 
-   long int someNum = 124558;
-   long long int something = 673875;
-   short int anotherSmolNumber = 69;
-
+    long int someNum = 124558;
+    long long int something = 673875;
+    short int anotherSmolNumber = 69;
 }
