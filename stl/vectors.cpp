a406bce38bb776f4fa76be54a8791eef1cdc1dd8
Shiva <shivaxwarrior@gmail.com>
2023-12-05 23:17:38 +0530

    added array

---
+++ stl/vectors.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // vector implementation
+    vector<int> v;
+    // A vector is a dynamic array. It is similar to an array but doesn't have a fixed size.
+    // It can grow or shrink its size.
+    // It is a sequence container and also known as a Dynamic Array or Array List.
+}
